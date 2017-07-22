b8390cbf1a5c217a9a72cdd187695c40aa0f91a1
Gabriel Teodoro <teodorohawking@outlook.com>
2017-07-22 14:20:10 -0300

    some more modifications

--- includes/footer.php
+++ includes/footer.php
@@ -3,6 +3,6 @@
         <span>@DIABETEENS | Tel: (92) 99174-4132</span>
     </div>
     <div class="col-lg-6 text-right">
-        <a href="mailto:contato@agenciaguppy.com.br">diabeteensbr@gmail.com</a>
+        <a href="mailto:diabeteensbr@gmail.com">diabeteensbr@gmail.com</a>
     </div>
 </footer>
